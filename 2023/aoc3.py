51d621297240bcd94da773edbc39d56ccfc4bcdf
Faboo21 <fabienleroy2004@gmail.com>
2023-12-05 00:47:07 +0100

    day3 V1 bug

--- 2023/aoc3.py
+++ 2023/aoc3.py
@@ -1,24 +1,34 @@
-"""import re
+from copy import copy
 
-with open("2023/input.txt", 'r') as fichier:
-    inputFinal = fichier.read()
-with open("2023/inputTest.txt", 'r') as fichier2:
-    inputTest = fichier2.read()
+with open("2023/input.txt", 'r') as fichier:input = fichier.read()
+with open("2023/inputTest.txt", 'r') as fichier:inputTest = fichier.read()
+
+def affichetab(tab):
+    for ligne in tab: print (ligne)
 
 def v1(entree):
+    input = entree.split("\n")
+    for i in range(len(input)):
+        input[i] = '.' + input[i] + '.'
+        for char in  ['+', '-', '*', '/', '(', ')', '@','%','&','=','$','#']:
+            input[i] = input[i].replace(char,'#')
+    input.insert(0,'.'*(len(input[0])))
+    input.append('.'*(len(input[0])))
     
-    entree = entree.split()
-    for i in range(len(entree)) :
-        tab = []
-        ligne = entree[i]
-        nb = [(int(s)) for s in re.findall(r"\d+", ligne)]
-        for nombre in nb :
-            tab.append([ligne.find(str(nombre)),nombre])
-        for couple in tab:
-            if (i > 0) :    
-                if any("#+*$" in entree[i-1][couple[0]-1::couple[0]+len(str(couple[1]))]) :
-                    print (entree[i-1][couple[0]-1:couple[0]+len(str(couple[1]))])
+    total = 0
+    for i in range(1,len(input)-1):
+        numbers = input[i].replace('.',' ').replace("#"," ")
+        numbers = [int(temp) for temp in numbers.split() if temp.isdigit()]
+        temp = copy(input[i])
+        for nombre in numbers:
+            print (temp)
+            taille = len(str(nombre))
+            pos = temp.find(str(nombre))
+            temp = temp.replace(str(nombre)," ")
             
-v1(inputTest)
-
-"""
+            #surement un pb de plusieurs occurence dans une liste
+            if "#" in input[i - 1][pos-1:pos+taille+1] or "#" in input[i][pos-1:pos+taille+1] or "#" in input[i + 1][pos-1:pos+taille+1]:
+                total += nombre
+    print (total)
+    
+v1(input)
